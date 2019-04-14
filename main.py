58b5152aad3920929d7b9d9b17928a0d959fa0f7
Janelle Burger <janelle.burger333@gmail.com>
2019-04-14 11:16:13 -0500

    Fully functioning User-signup

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from flask import Flask, request, redirect, render_template
 import os
-import jinja2
-
-template_dir = os.path.join(os.path.dirname(__file__),
-    'templates')
-
-jinja_env = jinja2.Environment(
-    loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
-
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -32,32 +24,70 @@ def validate_inputs():
     verify_password_error = ""
     email_error = ""
 
+
 #Validating username
     if " " in username or len(username) < 3 or len(username) > 20:
         username_error = "Not a valid username; must be 3-20 characters in length with no spaces"
-        
+        return render_template('inputs.html', username_error = username_error, email = email)
+
 #Validating password
     if " " in password or len(password) < 3 or len(password) > 20:
         password_error = "Not a valid password; must be 3-20 characters in length with no spaces"
         password =""
-
+        return render_template('inputs.html', username = username, 
+        password_error = password_error, 
+        email = email)
+    
 #Validating password verification
     if password != verify_password:
         verify_password_error = "Passwords do not match"
         verify_password = ""
+        return render_template('inputs.html', username = username, 
+        password_error = password_error, 
+        verify_password_error = verify_password_error, 
+        email = email)
+    
 
 #Validating email
-    if len(email) < 3 or len(email) > 20 and "@" not in email and "." not in email:
-        email_error = "Not a valid email:  make sure have only one '@' and one '.'"
+    if not(email):
+        pass
+    else: 
+        if email:
+            count_1 = 0
+            for char in email:
+                if char == "@":
+                    count_1 += 1
 
-#If everything passes, then will redirect to 'Welcome' page
+            if count_1 != 1:
+                email_error = "Not a valid email:  make sure have only one '@' and one '.'"
+                return render_template('inputs.html', username = username, password_error = password_error,
+                verify_password_error = verify_password_error, email_error = email_error)
+
+    
+            count_2 = 0
+            for char in email:
+                if char == ".":
+                    count_2 += 1
+
+            if count_2 != 1:
+                email_error = "Not a valid email:  make sure have only one '@' and one '.'"
+                return render_template('inputs.html', username = username, password_error = password_error,
+                verify_password_error = verify_password_error, email_error = email_error)
+            
+            if " " in email or len(email) < 3 or len(email) > 20:
+                email_error = "Not a valid email:  make sure have only one '@' and one '.'"
+                return render_template('inputs.html', username = username, password_error = password_error,
+                verify_password_error = verify_password_error, email_error = email_error)
+    
+#If everything passes, then will redirect to 'Welcome' page, else:  errors will be displayed
     if not username_error and not password_error and not verify_password_error and not email_error:
         username = request.form['username']
         return render_template('welcome.html', username=username)
-#If there are errors, will be displayed    
+
     else:
         return render_template('inputs.html', username_error = username_error, 
-        password_error = password_error, verify_password_error= verify_password_error,
+        password_error = password_error, 
+        verify_password_error = verify_password_error,
         email_error = email_error)
  
 
